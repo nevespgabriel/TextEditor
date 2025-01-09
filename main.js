227ca9d85b2b40b0ca3af2b139ae65fcfc35c61b
Gabriel Pereira Neves <157249929+nevespgabriel@users.noreply.github.com>
2025-01-09 01:32:37 -0300

    feat: font-size bug

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ const inputSize = document.getElementById('font-size');
 const dropdown = document.getElementById('dropdown');
 const btnPlus = document.getElementById('btn-plus');
 const btnMinus = document.getElementById('btn-minus');
+const editor = document.querySelector("#editor");
 
+// Exibe o dropdown ao clicar no input
 inputSize.addEventListener('click', function () {
     dropdown.style.display = 'block';
 });
+
+function changeSize(size){
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const span = document.createElement("span");
+
+    // Altera o tamanho da fonte selecionada para 20px
+    span.style.fontSize = size;
+    
+    range.surroundContents(span);
+}
 
 // Colocar para o font-size ser o selecionado
 document.addEventListener('click', function (e) {
@@ -19,6 +32,7 @@ dropdown.addEventListener('click', function (e) {
     if (e.target.dataset.value) {
         inputSize.value = e.target.dataset.value;
         dropdown.style.display = 'none';
+        changeSize(e.target.dataset.value)
     }
 });
 
@@ -26,12 +40,15 @@ dropdown.addEventListener('click', function (e) {
 btnPlus.addEventListener('click', function () {
     let currentValue = parseInt(inputSize.value) || 12;
     inputSize.value = currentValue + 1;
+    changeSize(inputSize.value);
 });
 
 btnMinus.addEventListener('click', function () {
     let currentValue = parseInt(inputSize.value) || 12;
     inputSize.value = currentValue - 1;
+    changeSize(inputSize.value);
 });
+
 
 document.getElementById('font-family').addEventListener('change', function () {
     const selectedFont = this.value;
@@ -65,3 +82,15 @@ document.addEventListener('click', (event) => {
     customSelect.classList.remove('active'); // Fecha o menu se clicar fora dele
   }
 });
+
+function textModify(modifier){
+    document.querySelector(`#${modifier}-button`).addEventListener("click", function() {
+        this.classList.toggle("pressed");
+        document.execCommand(modifier);
+    });
+}
+
+
+textModify("bold");
+textModify("italic");
+textModify("underline");
